e76a31e6b7f7c393b3c1d41d60fe9a977f3b92bf
Superbolt-X <67989957+superbolt-x@users.noreply.github.com>
2025-04-09 07:55:24 -0600

    Update blended_performance.sql

--- models/reporting/blended_performance.sql
+++ models/reporting/blended_performance.sql
@@ -1,5 +1,5 @@
 {{ config (
-    alias = target.database + '_blended'
+    alias = target.database + '_blended_performance'
 )}}
 
 {% set date_granularity_list = ['day', 'week', 'month', 'quarter', 'year'] %}
